8b40665ea753a18c3e138aa674442ffd0253ef7f
VanshAdhlakha <vanshadhlakha@Vansh-MacBook-Pro.local>
2026-04-01 22:25:47 +0530

    script 3

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ const resultEl = document.getElementById("result");
 
 btn.addEventListener("click", getFlights);
 
-
 async function getFlights() {
   statusEl.textContent = "loading...";
   resultEl.innerHTML = "";
 
+  try {
+    const res = await fetch("https://api.tvmaze.com/search/shows?q=air");
+    const data = await res.json();
+
+    if (!data || data.length === 0) {
+      statusEl.textContent = "no data";
+      return;
+    }
+  } catch (err) {
+    statusEl.textContent = "error";
+  }
 }
